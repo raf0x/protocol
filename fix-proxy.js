784773f5c94147dcf2c032c7f9b71229bc702189
raf0x <rlemor@gmail.com>
2026-04-12 12:24:17 -0400

    Fix proxy function export name

---
+++ fix-proxy.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('proxy.ts', 'utf8');
+content = content.replace('export async function middleware(', 'export async function proxy(');
+fs.writeFileSync('proxy.ts', content, 'utf8');
+console.log('Done');
