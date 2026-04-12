95a3339a3a3324ddb172a6dd553d5d1ec8092e0b
raf0x <rlemor@gmail.com>
2026-04-12 13:43:44 -0400

    Fix proxy matcher to exclude api routes

---
+++ fix-matcher2.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('proxy.ts', 'utf8');
+content = content.replace(
+  /matcher: \[.*\]/,
+  "matcher: ['/((?!_next/static|_next/image|favicon.ico|api/|.*\\.png$).*)']"
+);
+fs.writeFileSync('proxy.ts', content, 'utf8');
+console.log('Done');
