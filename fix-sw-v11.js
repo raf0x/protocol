324822bd698b3e87793a566f3dd75c1be73175aa
raf0x <rlemor@gmail.com>
2026-04-15 19:02:18 -0400

    Bump SW to v11

---
+++ fix-sw-v11.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('public/sw.js', 'utf8');
+content = content.replace(/protocol-v\d+/, "protocol-v11");
+fs.writeFileSync('public/sw.js', content, 'utf8');
+console.log('Done');
