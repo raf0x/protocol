cd9cd726232eedeb9b30c78596c07622d3a846e9
raf0x <rlemor@gmail.com>
2026-04-19 10:00:52 -0400

    Swap landing page screenshot to journal-screenshot2.png

---
+++ fix-screenshot-swap.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('app/page.tsx', 'utf8');
+content = content.replace(
+  "src='/journal-screenshot.png'",
+  "src='/journal-screenshot2.png'"
+);
+fs.writeFileSync('app/page.tsx', content, 'utf8');
+console.log('Done');
