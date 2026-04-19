5c7c80f9b6a69be076536e88a2bd0507cfdc3c7d
raf0x <rlemor@gmail.com>
2026-04-19 15:37:40 -0400

    Rename Manage stack to Add / Edit Protocols

---
+++ fix-manage-text.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('app/protocol/page.tsx', 'utf8');
+content = content.replace(
+  ">Manage stack →</a>",
+  ">+ Add / Edit Protocols →</a>"
+);
+fs.writeFileSync('app/protocol/page.tsx', content, 'utf8');
+console.log('Done');
